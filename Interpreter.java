a5be28cb5340b7785537483eb6819d1f8df75aae
fredaose <osei.freda@outlook.com>
2016-11-06 12:28:15 -0500

    Add files via upload

---
+++ Interpreter.java
@@ -0,0 +1,66 @@
+
+ 
+
+import java.util.Scanner;
+
+
+public class Interpreter {
+
+    private static GameState state; // not strictly necessary; GameState is 
+                                    // singleton
+
+    public static String USAGE_MSG = 
+        "Usage: Interpreter borkFile.bork|saveFile.sav.";
+
+    public static void main(String args[]) {
+
+        if (args.length < 1) {
+            System.err.println(USAGE_MSG);
+            System.exit(1);
+        }
+
+        String command;
+        Scanner commandLine = new Scanner(System.in);
+
+        try {
+            state = GameState.instance();
+            if (args[0].endsWith(".bork")) {
+                state.initialize(new Dungeon(args[0]));
+                System.out.println("\nWelcome to " + 
+                    state.getDungeon().getName() + "!");
+            } else if (args[0].endsWith(".sav")) {
+                state.restore(args[0]);
+                System.out.println("\nWelcome back to " + 
+                    state.getDungeon().getName() + "!");
+            } else {
+                System.err.println(USAGE_MSG);
+                System.exit(2);
+            }
+
+            System.out.print("\n" + 
+                state.getAdventurersCurrentRoom().describe() + "\n");
+
+            command = promptUser(commandLine);
+
+            while (!command.equals("q")) {
+
+                System.out.print(
+                    CommandFactory.instance().parse(command).execute());
+
+                command = promptUser(commandLine);
+            }
+
+            System.out.println("Bye!");
+
+        } catch(Exception e) { 
+            e.printStackTrace(); 
+        }
+    }
+
+    private static String promptUser(Scanner commandLine) {
+
+        System.out.print("> ");
+        return commandLine.nextLine();
+    }
+
+}
