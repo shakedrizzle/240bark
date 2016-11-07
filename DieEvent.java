2c52323126b81d62b8596400f906103d59b7fb1e
shakedrizzle <shakedrizzle@yahoo.com>
2016-11-07 00:50:14 -0500

    Add files via upload

---
+++ DieEvent.java
@@ -0,0 +1,22 @@
+
+/**
+ * Subclass of Event that ends the game if health reaches 0.
+ * 
+ * @author Matt Rychlik
+ * @version 11/06/2016
+ */
+public class DieEvent extends Event
+{
+    private boolean death;
+    
+    /**
+     * Constructor for objects of class DieEvent
+     */
+    public DieEvent(){}
+
+    /**
+     * Kills player and ends game
+     */
+    void execute(){}
+
+}
