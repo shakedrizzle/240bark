7af6387313746f18d798f403212d27f64e178923
fredaose <osei.freda@outlook.com>
2016-11-11 21:05:10 -0500

    Update HealthCommand.java

--- HealthCommand.java
+++ HealthCommand.java
@@ -16,5 +16,23 @@ public class HealthCommand extends Command{
      * 
      * @return string depending on current health
      */
-    String execute(){return null;}
+    String execute(){
+       int health = GameState.instance().getHealth();
+        if(health>=75){
+            return ("You are in good health with lots of energy!");
+        }else if(health>=50){
+            return ("You are not very strong, its probably one of those days");
+        }else if(health>=25){
+            return("I suggest you think of plan B because you are almost out of health!");
+        }else if(health>=10){
+            return("Dudeeee, act on plan B!, you are about to faint");
+        }else if(health>=5){
+            return("You've got one drip on blood in you, you are lucky to be standing");
+        }else if(health>=0){
+            return("Adios amigo/amiga, you are dying!");
+        }
+        else{
+            return null;
+        } 
+    }
 }
