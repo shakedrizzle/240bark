1c9b05f3af9f4258bd7f135d7586fdf879998661
flmngrose <rconnoll@umw.edu>
2016-11-22 13:46:05 -0500

    Add files via upload

---
+++ UnknownEvent.java
@@ -0,0 +1,14 @@
+
+/**
+ * incase something goes wrong
+ * to avoid null pointer exceptions
+ * 
+ * @author Rose 
+ * @version 1.0
+ */
+public class UnknownEvent extends Event
+{
+    String execute() {
+        return "Bork \n";
+    }
+}
