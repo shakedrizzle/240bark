78e6b81a4561ae83f2cf5278bc328204479a8fa1
flmngrose <rconnoll@umw.edu>
2016-11-21 20:33:36 -0500

    Update TeleportEvent.java

--- TeleportEvent.java
+++ TeleportEvent.java
@@ -1,21 +1,41 @@
-
+import java.util.*;
 /**
- * Subclass of Event that changes the current room of gamestate.
+ * Subclass of Command that changes the current room of the GameState
  * 
- * @author Freda Osei
- * @version Nov 6, 2016
+ * @author Rose 
+ * @version 1.1
  */
-public class TeleportEvent extends Event
+public class TeleportCommand extends Command
 {
-    private String place;
-    
+    private Room place;
+    boolean tripped=false;
     /**
      * Constructor for objects of class TeleportEvent
+     * @param  name  of the room desired
      */
-     TeleportEvent(String name){}
+    TeleportCommand(String name){
+        if ( GameState.instance().getDungeon().getHashtable().containsKey(name)){
+            place = GameState.instance().getDungeon().getRoom(name);
+        }
+        else{     
+            tripped = true;
+        }
+    }
 
     /**
      * Changes the current room to another room.
+     * 
      */
-    void execute(){}
+    String execute(){//change adventerers current room to the room that is being teleported too
+        //set adventures current room to place
+        if(GameState.instance().getTele()&&!tripped){
+            GameState.instance().setAdventurersCurrentRoom(place);
+            String desc = place.describe();
+            return desc;
+        }
+        else if(tripped){return "that is not a real room dog breath";}
+        else{
+            return "you do not hold the item needed for this event";
+        }
+    }
 }
