f5b5684ca4e2f233ccef40ee0a5113abf0bdd8be
fredaose <osei.freda@outlook.com>
2016-11-22 21:15:30 -0500

    Update EventFactory.java

--- EventFactory.java
+++ EventFactory.java
@@ -9,6 +9,7 @@ import java.util.*;
 public class EventFactory
 {
     public static EventFactory theInstance;
+    private Hashtable<String,String> events;
     /**
      * method that creates an EventFactory if none exists  
      * 
@@ -29,11 +30,28 @@ public class EventFactory
      * @return the Event that is called
      */
     public Event parse(String event, Item name){
+	String first = "";
+	events = name.getEventCommands();
+	if (!events.isEmpty()) {
+            String Everbs = events.get(event);
+            String[] Enouns = Everbs.split(",");
+            for(String word:Enouns){
+                int firstbananna = word.indexOf("(");
+                int lastbananna = word.indexOf(")");
+		 if(word.contains("(")){
+                    first = word.substring(0,firstbananna);
+                }else
+                first=word;
+	    
         if(event.contains("Die")){return new DieEvent();}
         if(event.contains("Disappear")){return new DisappearEvent();}
         if(event.contains("Win")){return new WinEvent();}
-        if(event.contains("Score")){return new ScoreEvent(num);}
+        if(event.contains("Score")){return new ScoreEvent(Integer.valueOf(word.substring(firstbananna,lastbananna)));}
+	if(event.contains("Wound")){return new WoundEvent(Integer.valueOf(word.substring(firstbananna,lastbananna)));}
+	    }
+	}
         return new UnknownEvent();
+	        
     }
 	    
 }
