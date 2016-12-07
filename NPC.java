824d6384b4e3eca29b039c3e40d4b4d145848e9f
shakedrizzle <shakedrizzle@yahoo.com>
2016-12-07 16:55:05 -0500

    Add files via upload

--- NPC.java
+++ NPC.java
@@ -11,6 +11,15 @@ public class NPC
     private String NPCname;
     private boolean friend;
     private Hashtable <String,String> dialog;
+    private int health;
+    private int attack;
+    private int defense;
+    private boolean isAlly;
+    private ArrayList<Item> inventory;
+    private String desc;
+    private String name;
+    
+    
     /**
      * Constructor for objects of class NPC
      * accesses file for NPCs
@@ -18,8 +27,19 @@ public class NPC
      * @param  s  where the method will read the information it needs to fill & complete
      * the NPC
      */
-    NPC(Scanner s) throws NoNPCException,
-    Dungeon.IllegalDungeonFormatException {}
+    NPC(int health, int attack, int defense, boolean isAlly, String desc, String name) throws NoNPCException,
+    Dungeon.IllegalDungeonFormatException 
+    {
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+        this.isAlly = isAlly;
+        inventory = new ArrayList<Item>();
+        this.desc = desc;
+        this.name = name;
+        
+    
+    }
     
     /**
      * Makes sure the NPC goes by the specified name (could have other borking aliases)
@@ -44,4 +64,55 @@ public class NPC
      * @return  String  of the borks our NPC will give, in other words, the NPC's dialog
      */
     public String getDialog(String borks){return dialog.get(borks);}
+    
+    public void setDialog(String desc){
+        int temp = desc.indexOf(":");
+        String action = desc.substring(0,temp);
+        String phrase = desc.substring(temp+1);
+        dialog.put(action, phrase);
+        
+    }
+    
+    public String takeHit(int damage){
+        if (damage > defense){
+            this.health = this.health - (damage - defense);
+            return this.name+" took damage. Their health is now "+this.health+".\n";
+        }
+        else 
+            this.health = this.health - (damage / 2);
+            return this.name+"'s defense was so strong they took half damage. Their health is now "+this.health+".\n";
+                    
+    }
+    
+    public String die(){
+        this.health = 0;
+        return this.name+" has died\n";
+    }
+    
+    public String dropInventory(){
+        for (Item item : inventory) {
+            String name = item.toString();
+            System.out.println(this.name+" has dropped "+item.goesBy(name)+"\n");
+            removeFromInventory(item);      
+        }
+        return "";
+    }
+    
+    public String addToInventory(Item item){
+        inventory.add(item);
+        return "Added "+item.toString()+"\n";
+        
+    }
+    public String removeFromInventory(Item item){
+        inventory.remove(item);
+        return "Removed "+item.toString()+"\n";
+    }
+    
+    public String getInventory(){
+        for (Item item : inventory) {
+            String name = item.toString();
+            System.out.println(name);     
+        }
+        return "";
+    }
 }
